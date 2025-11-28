6514b6f796237f1bce5cd8d87833ff78a513c124
chrishan0321 <hant20110321@gmail.com>
2025-11-28 13:18:16 +0000

    delete api

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-# core/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('important_notices/', views.important_notices, name='important_notices'),
-    path('normal_notices/', views.normal_notices, name='normal_notices'),
-    path('archived_notices/', views.archived_notices, name='archived_notices'),
-]
